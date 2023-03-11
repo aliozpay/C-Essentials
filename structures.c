66d6363e96aa7314e1a40ea5e1c8fc95fce051fd
Aozpay <alicanozpay@gmail.com>
2023-03-11 13:44:08 +0300

    Arrays and structures codes are written

---
+++ structures.c
@@ -0,0 +1,19 @@
+/*
+*/
+
+struct Rectangle
+{
+    int length;
+    int width;
+};
+
+#include <stdio.h>
+int main(){ 
+    
+    struct Rectangle r1 = {20,30}; //Initial conditions
+    printf("Size of structure is : %d\nRectangle's length : %d\nRectangle's width : %d",sizeof(r1),r1.length,r1.width);
+
+    
+
+    
+}
