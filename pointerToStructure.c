58a0357145f5a85c9471d3fafd5871429b51ec50
Aozpay <alicanozpay@gmail.com>
2023-03-12 19:55:25 +0300

    2 example is added to pointerToStructure code

--- pointerToStructure.c
+++ pointerToStructure.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct rectangle{
+
+    int length;
+    int width;
+};
 int main()
 {
 
-    
+    /* 
+        Ex1: Accesing struct's variable over the pointer
+        We used (*p).length instead of p->length.
+    */
+
+    struct rectangle r1 = {10,5};
+    struct rectangle *p;
+    struct rectangle *p2;
+    p = &r1;
+    printf("Length value is : %d\n",p->length);
+    printf("Width value is : %d\n ",p->width);
+
+    /*
+        Ex2: Dynamic memory using with structures and accesing over the heap
+    */
+    p2 = (struct rectangle *)malloc(sizeof(struct rectangle));
+    p2->length = 100;
+    p2->width = 200;
+    printf("Ex2: Reading structure values from heap\n");
+    printf("Length : %d\n",p2->length);
+    printf("Width : %d\n",p2->width);
+    free(p2);
 }
